c7762df661a0d3665c317e1d404d5de06d72b1aa
PierreMarchetti <pierre.marh@gmail.com>
2018-12-04 16:15:26 +0100

    navbar and small article implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbdCarouselBasic } from './big-img/big-img.component';
+import {SmallImgComponent} from './small-img/small-img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    NgbdCarouselBasic
+    NgbdCarouselBasic,
+    SmallImgComponent
   ],
   imports: [
     BrowserModule,
